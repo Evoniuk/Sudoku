04f56bb0e82a404df355868e400d3f19e30a07da
evoniuk <jack.evoniuk@gmail.com>
2021-03-05 22:56:39 -0800

    Updates

---
+++ precache-manifest.d4ee6a0c845251d1402afa369772e763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88e17ced0bc882a65f24308c287c885",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "06ee61d8ffee939c0fe0",
+    "url": "/Sudoku/static/css/main.2ef2e293.chunk.css"
+  },
+  {
+    "revision": "17233ed2763173cff836",
+    "url": "/Sudoku/static/js/2.ae55532c.chunk.js"
+  },
+  {
+    "revision": "06ee61d8ffee939c0fe0",
+    "url": "/Sudoku/static/js/main.fa174e73.chunk.js"
+  },
+  {
+    "revision": "8030cc24d71d93af4e4d",
+    "url": "/Sudoku/static/js/runtime-main.9aab62cb.js"
+  }
+]);
